deb84e794ace4f940879217684de2ece5a15eaa9
MZD <michael@caracolazul.dev>
2024-08-27 12:39:27 -0400

    errorMsg function for consistent handling of Ctrl+D (EOF);

--- add-config.go
+++ add-config.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io"
 	"os"
@@ -14,20 +15,29 @@ func prompt(s string) {
 	fmt.Print(s)
 }
 
+func errorMsg(tpl string, err error) {
+	if tpl == "" {
+		tpl = "Error reading input: %v\n"
+	}
+
+	if err != nil {
+		if errors.Is(err,io.EOF){
+			fmt.Println("\nExiting. Goodbye!")
+			return
+		}
+		fmt.Fprintf(os.Stderr, tpl, err)
+		return
+	}
+}
+
 func main() {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Println("Add config to a file. Type Ctrl+D to quit.")
 
 	prompt("\nFile-name? ")
 	filename, err := reader.ReadString('\n')
-	if err != nil {
-		if err == io.EOF {
-			fmt.Println("\nExiting the CLI. Goodbye!")
-			return
-		}
-		fmt.Println("Error reading input:", err)
-		return
-	}
+	errorMsg("Error reading input: %v\n", err)
+
 	filename = strings.TrimSpace(filename)
 
 	if filename == "" {
@@ -40,19 +50,13 @@ func main() {
 	filename += ".tpl"
 
 	file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error opening file: %v\n", err)
-		return
-	}
+	errorMsg("Error opening file: %v\n", err)
 	defer file.Close()
 
 	for {
 		prompt("\nConfig Name? ")
 		name, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error reading config name: %v\n", err)
-			return
-		}
+		errorMsg("Error reading config name: %v\n", err)
 		name = strings.TrimSpace(name)
 
 		if name == "" {
@@ -61,18 +65,12 @@ func main() {
 
 		prompt("Help text? ")
 		help, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error reading help text: %v\n", err)
-			return
-		}
+		errorMsg("Error reading help text: %v\n", err)
 		help = strings.TrimSpace(help)
 
 		prompt("export? [yes] ")
 		noExp, err := reader.ReadString('\n')
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error reading export option: %v\n", err)
-			return
-		}
+		errorMsg("Error reading export option: %v\n", err)
 		noExp = strings.TrimSpace(noExp)
 
 		prefix := ""
@@ -82,10 +80,7 @@ func main() {
 
 		line := fmt.Sprintf("%s%s ?= {{%s}}# %s\n", prefix, name, name, help)
 		_, err = file.WriteString(line)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Error writing to file: %v\n", err)
-			return
-		}
+		errorMsg("Error writing to file: %v\n", err)
 
 		fmt.Printf("%s%s ?= {{%s}}# %s >> %s\n", prefix, name, name, help, filename)
 	}
